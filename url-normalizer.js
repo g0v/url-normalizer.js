fdfa93f6472079f99b7866ec118ea8bacd4b025d
Ronny Wang <ronnywang@gmail.com>
2013-12-30 10:26:40 +0800

    add www.appledaily.com.tw

--- url-normalizer.js
+++ url-normalizer.js
@@ -6,6 +6,23 @@
     ret.query_url = url;
 
     switch (url_parts['hostname']) {
+    case 'www.appledaily.com.tw':
+      // Ex: http://www.appledaily.com.tw/realtimenews/article/finance/20131225/314703/【台股開盤】開高上漲11點
+      // /{appledaily|realtimenews} 不能換掉
+      // /article
+      // /{$category} 3c, beauty, entertainment ... 可以任意更換
+      // /YYYYMMDD 日期，不能任意更換
+      // /{$ID}
+      // /{$TITLE} 可以拿掉
+      var pathname_parts = url_parts['pathname'].split('/');
+      if ('article' != pathname_parts[2]) {
+        // 不是新聞網址
+        break;
+      }
+
+      ret.normalized_url = 'http://www.appledaily.com.tw/' + pathname_parts[1] + '/article/local/' + pathname_parts[4] + '/' + pathname_parts[5];
+      ret.normalized_id = 'www.appledaily.com.tw/' + pathname_parts[1] + '/' + pathname_parts[4] + '/' + pathname_parts[5];
+      return ret;
     }
 
     return null;
