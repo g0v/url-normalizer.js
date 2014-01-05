65dec031a668a1946115bc6738741af97fcc097f
Ronny Wang <ronnywang@gmail.com>
2014-01-05 23:52:22 +0800

    URLNormalizer.query() 改成用 cb 的方式

--- node-test.js
+++ node-test.js
@@ -1,7 +1,7 @@
 var URLNormalizer = require('./url-normalizer.js');
 
 var url = 'http://www.appledaily.com.tw/realtimenews/article/finance/20131225/314703/【台股開盤】開高上漲11點';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'http://www.appledaily.com.tw/realtimenews/article/finance/20131225/314703/【台股開盤】 開高上漲11點',
   normalized_url: 'http://www.appledaily.com.tw/realtimenews/article/local/20131225/314703',
@@ -9,7 +9,7 @@ console.log(URLNormalizer.query(url));
 */
 
 url = 'http://www.chinatimes.com/newspapers/蠻牛再起-3登年終球王-20131108000743-260111?from=fb';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'http://www.chinatimes.com/newspapers/蠻牛再起-3登年終球王-20131108000743-260111?from=fb',
   normalized_url: 'http://www.chinatimes.com/newspapers/20131108000743-260111',
@@ -17,21 +17,21 @@ console.log(URLNormalizer.query(url));
 */
 
 url = 'http://www.cna.com.tw/News/aFE/201309070021-1.aspx';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'http://www.cna.com.tw/News/aFE/201309070021-1.aspx',
   normalized_url: 'http://www.cna.com.tw/News/aAll/201309070021-1.aspx',
   normalized_id: 'www.cna.com.tw/aAll/201309070021-1.aspx' }
 */
 url = 'http://www.cna.com.tw/News/firstnews/201309070021-1.aspx';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'http://www.cna.com.tw/News/firstnews/201309070021-1.aspx',
   normalized_url: 'http://www.cna.com.tw/News/firstnews/201309070021-1.aspx',
   normalized_id: 'www.cna.com.tw/firstnews/201309070021-1.aspx' }
 */
 url = 'http://www.cna.com.tw/Topic/Popular/3959-1/201309070021-1.aspx';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'http://www.cna.com.tw/Topic/Popular/3959-1/201309070021-1.aspx',
   normalized_url: 'http://www.cna.com.tw/News/firstnews/201309070021-1.aspx',
@@ -39,7 +39,7 @@ console.log(URLNormalizer.query(url));
 */
 
 url = 'http://www.nownews.com/n/2013/10/28/1003767';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'http://www.nownews.com/n/2013/10/28/1003767',
   normalized_url: 'http://www.nownews.com/n/2013/10/28/1003767',
@@ -47,7 +47,7 @@ console.log(URLNormalizer.query(url));
 */
 
 url = 'http://newtalk.tw/news/2013/12/30/43243.html';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'http://newtalk.tw/news/2013/12/30/43243.html',
   normalized_url: 'http://newtalk.tw/news/2013/12/30/43243.html',
@@ -55,14 +55,14 @@ console.log(URLNormalizer.query(url));
 */
 
 url = 'http://iservice.libertytimes.com.tw/liveNews/news.php?no=927070&type=%E5%9C%8B%E9%9A%9B';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'http://iservice.libertytimes.com.tw/liveNews/news.php?no=927070&type=%E5%9C%8B%E9%9A%9B',
   normalized_url: 'http://iservice.libertytimes.com.tw/liveNews/news.php?no=927070',
   normalized_id: 'iservice.libertytimes.com.tw/liveNews/927070' }
 */
 url = 'http://www.libertytimes.com.tw/2013/new/dec/30/today-taipei1.htm';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'http://www.libertytimes.com.tw/2013/new/dec/30/today-taipei1.htm',
   normalized_url: 'http://www.libertytimes.com.tw/2013/new/dec/30/today-taipei1.htm',
@@ -70,7 +70,7 @@ console.log(URLNormalizer.query(url));
 */
 
 url = 'http://www.ettoday.net/news/20131106/291629.htm';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'http://www.ettoday.net/news/20131106/291629.htm',
   normalized_url: 'http://www.ettoday.net/news/20131106/291629.htm',
@@ -78,7 +78,7 @@ console.log(URLNormalizer.query(url));
 */
 
 url = 'http://tw.news.yahoo.com/敘利亞沙林原料-英廠商提供的-044631577.html';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'http://tw.news.yahoo.com/敘利亞沙林原料-英廠商提供的-044631577.html',
   normalized_url: 'http://tw.news.yahoo.com/敘利亞沙林原料-英廠商提供的-044631577.html',
@@ -86,21 +86,21 @@ console.log(URLNormalizer.query(url));
 */
 
 url = 'https://www.facebook.com/photo.php?fbid=187340678125996&set=a.169476096579121.1073741830.100005501943040&type=1&theater';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'https://www.facebook.com/photo.php?fbid=187340678125996&set=a.169476096579121.1073741830.100005501943040&type=1&theater',
   normalized_url: 'https://www.facebook.com/photo.php?fbid=187340678125996',
   normalized_id: 'www.facebook.com/photo/187340678125996' }
 */
 url = 'https://www.facebook.com/394896373929368/posts/552832544802416';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'https://www.facebook.com/394896373929368/posts/552832544802416',
   normalized_url: 'https://www.facebook.com/394896373929368/posts/552832544802416',
   normalized_id: 'www.facebook.com/posts/552832544802416' }
 */
 url = 'https://www.facebook.com/permalink.php?story_fbid=691671064186291&id=100000302107473';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'https://www.facebook.com/permalink.php?story_fbid=691671064186291&id=100000302107473',
   normalized_url: 'https://www.facebook.com/permalink.php?story_fbid=691671064186291&id=100000302107473',
@@ -108,21 +108,21 @@ console.log(URLNormalizer.query(url));
 */
 
 url = 'http://udn.com/NEWS/BREAKINGNEWS/BREAKINGNEWS1/民進黨立委杯葛-院會無議程-8395297.shtml';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'http://udn.com/NEWS/BREAKINGNEWS/BREAKINGNEWS1/民進黨立委杯葛-院會無議程-8395297.shtml',
   normalized_url: 'http://udn.com/NEWS/BREAKINGNEWS/BREAKINGNEWS1/8395297.shtml',
   normalized_id: 'udn.com/news/8395297' }
 */
 url = 'http://forum.udn.com/forum/NewsLetter/NewsPreview?Encode=Big5&NewsID=8108263&ch=fb_share&R=346';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'http://forum.udn.com/forum/NewsLetter/NewsPreview?Encode=Big5&NewsID=8108263&ch=fb_share&R=346',
   normalized_url: 'http://forum.udn.com/forum/NewsLetter/NewsPreview?Encode=Big5&NewsID=8108263',
   normalized_id: 'udn.com/news/8108263' }
 */
 url = 'http://www.udn.com/2013/10/31/NEWS/NATIONAL/NATS1/8263682.shtml?ch=fb_share';
-console.log(URLNormalizer.query(url));
+URLNormalizer.query(url, function(d){ console.log(d); });
 /*
 { query_url: 'http://www.udn.com/2013/10/31/NEWS/NATIONAL/NATS1/8263682.shtml?ch=fb_share',
   normalized_url: 'http://www.udn.com/2013/10/31/NEWS/NATIONAL/NATS1/8263682.shtml',
